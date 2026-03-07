b8ef9814931dea1a03b5d707cd2d599dfd3e45fb
aegorsuch <alex@weartak.com>
2026-03-07 06:22:02 -0600

    test assert

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    environment: 'node', // or 'jsdom' if DOM is needed
+    globals: true,
     server: {
       deps: {
         inline: ["C:"]
